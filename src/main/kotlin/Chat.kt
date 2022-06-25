49181f90286a45a5810add969a736cf26b06fe95
Евгений <eugeny1988@gmail.com>
2022-06-25 17:30:03 +0300

    Made constructor

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -1,16 +1,36 @@
 class Chat(
+    val id: Int,
     val user1: User,
     val user2: User,
-    var messages: MutableList<Message>
+    var messages: MutableList<Message>,
+    var isChatRead: Boolean,
+    var isLastMessage: Boolean
 ) {
-    fun createMessage(message: Message){
+
+    init {
+        for (message: Message in messages){
+            if (message.isRead){
+                isChatRead = true
+                break
+            } else isChatRead = false
+        }
+        if (!messages.isEmpty()){
+            isLastMessage = true
+        } else isLastMessage = false
+    }
+    fun createMessage(message: Message) {
         this.messages.add(message)
         message.isRead = true
 
     }
-    fun editMessage(id: Int, text: String){
-        this.messages.set(id-1,Message(text, false, true))
 
+    fun editMessage(id: Int, text: String) {
+        this.messages.set(id - 1, Message(text, false, true))
+
+    }
+    fun deleteMessage(id: Int){
+        this.messages.removeAt(id-1)
+        messages[id-1].isDeleted = true
     }
 
 
